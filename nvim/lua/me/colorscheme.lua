72930192c39d2eaada9484a6d7e81c6627f17b31
Paulius Krasnovas <paulius@krasnovas.com>
2022-02-22 18:15:39 +0100

    added colorscheme

---
+++ nvim/lua/me/colorscheme.lua
@@ -0,0 +1,8 @@
+local colorscheme = "ayu"
+vim.g.ayu_mirage = true
+
+local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
+if not status_ok then
+  vim.notify("colorscheme " .. colorscheme .. " not found!")
+  return
+end
